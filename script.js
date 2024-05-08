71e1568ff637c0b832d83b26a2aaa84fce9bafcb
Jeevith-Raj <20uad028@kamarajengg.edu.in>
2024-05-08 18:25:17 -0400

    this is also a commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let btnref = document.querySelectorAll(".button-option");
+let popupref = document.querySelector(".popup hide")
+let newgameBtn = docmuent.getElementByid("new-game");
+let restartBtn = document.getElementById("restart");
+let msgref = document.getElementById("message");
+
+let winningpattern =[[0,1,2],[0,3,6],[2,5,8],[6,7,8],[3,4,5],[1,4,7],[0,4,8],[2,4,6]];
